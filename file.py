5b976b3697d1e3e179f793fb7fc218196141f726
Wendy Quarshie <42671174+codebuggg@users.noreply.github.com>
2022-10-14 13:26:22 +0200

    adding a comment

--- file.py
+++ file.py
@@ -1,15 +1,7 @@
 
-
-<<<<<<< HEAD
-def potatoe(a, b);
-    return a - b
-
-def carrot(a, b)
-    return a + b
-=======
+#adding a comment jsut because ...
 def corn(a, b);
     return a + b
 
 def broccoli(a, b)
     return a + b
->>>>>>> 4acf34a53649d3ddfa92ba40f61f0c3a2b4f787c
